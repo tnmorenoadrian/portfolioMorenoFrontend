4c60f305ae4de2a4d72de3fc250e99e623d58867
tnmorenoadrian <tnmorenoadrian@gmail.com>
2022-09-22 23:42:18 -0300

    Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, MatCardModule ],
+  declarations: [ AppComponent, EncabezadoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
