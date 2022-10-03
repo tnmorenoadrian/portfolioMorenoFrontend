1e11ed5bd332c179ad2455f64d7e05d5b45180f4
tnmorenoadrian <tnmorenoadrian@gmail.com>
2022-10-02 22:06:16 -0300

    ver instalacion para modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './componentes/modal/modal.component';
 
 @NgModule({
   imports:      [ BrowserModule,
@@ -40,6 +41,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
                   ProyectosComponent,
                   LoginComponent,
                   MainComponent,
+                  ModalComponent,
                 ],
   providers: [    
                 ],
