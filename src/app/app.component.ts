96981b59f3696a7a1801081ed262441eddeb454e
tnmorenoadrian <tnmorenoadrian@gmail.com>
2022-11-22 11:53:29 -0300

    tema transparente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalImgFondoComponent } from './componentes/modal-img-fondo/modal-img-fondo.component';
+import { Persona } from './models/persona.model';
+import { AuthService } from './servicios/auth.service';
+import { PortfolioService } from './servicios/portfolio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolioMorenoFrontend';
+  miPortfolio: any;
+  styles: any;
 
-  constructor(
+  constructor(private datosPortfolio:PortfolioService,
+    private modalService: NgbModal,
+    public authService: AuthService
   ) {
   }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatosPersona().subscribe((data: Persona[]) =>{
+    this.miPortfolio=data;
+    this.showImgBack(); 
+    });
+    
+  }
+
+  showImgBack(){
+    const bgImageUrl = this.miPortfolio.image_background;
+    this.styles = {
+    backgroundImage: `url(${bgImageUrl})`,
+    
+       };  
+  }
+
+  openModalImgFondo() {
+    const modalRef = this.modalService.open(ModalImgFondoComponent,
+      {
+        scrollable: true,
+        windowClass: 'myCustomModalClass',
+        // keyboard: false,
+        // backdrop: 'static'
+      });
+
+    let title = "Imagen de fondo"
+    let datos_persona =this.miPortfolio
+
+    modalRef.componentInstance.fromParentPersona = datos_persona;
+    modalRef.componentInstance.fromParentTitle = title;
+    modalRef.result.then((result) => {
+      if(result){
+      this.datosPortfolio.actualizarDatos(result.id, result).subscribe((data) => {
+        this.miPortfolio.id = data.id;
+        this.showImgBack(); 
+      });
+      }
+    }).catch(() => { /* closed */ });
+  }
+   
 
 }
--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -3,7 +3,6 @@ import { PortfolioService } from '../../servicios/portfolio.service';
 import { AuthService } from '../../servicios/auth.service';
 import {Persona} from '../../models/persona.model'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalImgFondoComponent } from '../modal-img-fondo/modal-img-fondo.component';
 import { ModalLoginComponent } from '../modal-login/modal-login.component';
 
 
@@ -24,48 +23,17 @@ export class EncabezadoComponent implements OnInit {
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatosPersona().subscribe((data: Persona[]) =>{
-    this.miPortfolio=data;
-    this.showImgBack(); 
-    });
-    
+    this.miPortfolio=data
+    });  
   }
 
-  showImgBack(){
-    const bgImageUrl = this.miPortfolio.image_background;
-    this.styles = {
-    backgroundImage: `url(${bgImageUrl})`,
-    
-       };  
-  }
    
   logout() {
     console.log("you are logout")
     this.authService.logOut() 
     }
 
-    openModalImgFondo() {
-      const modalRef = this.modalService.open(ModalImgFondoComponent,
-        {
-          scrollable: true,
-          windowClass: 'myCustomModalClass',
-          // keyboard: false,
-          // backdrop: 'static'
-        });
-  
-      let title = "Imagen de fondo"
-      let datos_persona =this.miPortfolio
-  
-      modalRef.componentInstance.fromParentPersona = datos_persona;
-      modalRef.componentInstance.fromParentTitle = title;
-      modalRef.result.then((result) => {
-        if(result){
-        this.datosPortfolio.actualizarDatos(result.id, result).subscribe((data) => {
-          this.miPortfolio.id = data.id;
-          this.showImgBack(); 
-        });
-        }
-      }).catch(() => { /* closed */ });
-    }
+    
 
     openModalLogin() {
       const modalRef = this.modalService.open(ModalLoginComponent,
